4c639fe0d730870da78d6c66a4c1f053a8f8e110
Sadguru Chenu <sadgurustreak@gmail.com>
2026-03-29 18:52:34 +0530

    Added dynamic backend url into axios

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000',
+    baseURL: process.env.BACKEND_URL || 'http://127.0.0.1:8000',
 });
 
 // Add a request interceptor
